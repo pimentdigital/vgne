5c164801b3339b044a35f1be6ab64bb0207ef7f2
Mac Oosthuizen <m@m-jo.co>
2015-12-22 12:50:04 +0100

    updated dependencies and a start on the category listing cube on the home page

--- web/app/themes/catch-gare2/index.php
+++ web/app/themes/catch-gare2/index.php
@@ -34,16 +34,30 @@ get_header(); ?>
 			<?php while ( have_posts() ) : the_post(); ?>
 
 				<?php
+                    static $count = 0;
+
+                    if ($count == "3") { break; }
+                    else {
 					/* Include the Post-Format-specific template for the content.
 					 * If you want to override this in a child theme then include a file
 					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
 					 */
 					get_template_part( 'content', get_post_format() );
+
+                    $count++; }
 				?>
 
 			<?php endwhile; ?>
 
-			<?php catchresponsive_content_nav( 'nav-below' ); ?>
+			<?php //catchresponsive_content_nav( 'nav-below' ); ?>
+
+            <article class="post">
+                <div class="archive-post-wrap">
+                    <div class="entry-container">
+                        hello
+                    </div>
+                </div>
+            </article>
 
 		<?php else : ?>
 
